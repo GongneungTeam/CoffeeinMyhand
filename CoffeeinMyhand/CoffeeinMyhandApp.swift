aacd69adbf1e6aac28b7ac0cf06ae64d7a000a44
junghs0226 <36288627+junghs0226@users.noreply.github.com>
2021-05-15 00:25:16 +0900

    Initial Commit

---
+++ CoffeeinMyhand/CoffeeinMyhandApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeinMyhandApp.swift
+//  CoffeeinMyhand
+//
+//  Created by Hyeong Seok Jung on 2021/05/15.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeinMyhandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
